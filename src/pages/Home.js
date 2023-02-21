488fe8cfe2d0aec6127d6e89d9050efea9cac7fe
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-20 20:21:03 -0600

    Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
 // Name folder pages to show that the components inside of it will be loaded with the help of routing
 function HomePage() {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        {/* ahref loses context, or application-wide state as it sends a new request to the server */}
+        {/* listens for clicks & prevents browser default of sending http request, based on the route definitions loads the proper content/components */}
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
