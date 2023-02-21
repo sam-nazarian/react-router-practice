898e18ea34eed55255409ebd1c10865c758ecb93
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-21 13:50:14 -0600

    Navigating Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 // Name folder pages to show that the components inside of it will be loaded with the help of routing
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products');
+  }
+
   return (
     <>
       <h1>My Home Page</h1>
@@ -9,6 +15,9 @@ function HomePage() {
         {/* ahref loses context, or application-wide state as it sends a new request to the server */}
         {/* listens for clicks & prevents browser default of sending http request, based on the route definitions loads the proper content/components */}
         Go to <Link to="/products">the list of products</Link>
+        <p>
+          <button onClick={navigateHandler}>Navigate</button>
+        </p>
       </p>
     </>
   );
